c3250f43a345ba055e9db80fa7af86bc47ecaa68
Péter Blahó <warloxk@gmail.com>
2017-07-14 21:45:07 +0200

    initial commit

---
+++ cache.php
@@ -0,0 +1,15 @@
+<?
+/**
+ * drakoon-php cache manager
+ * @author  Peter Blaho <info@peterblaho.com>
+ * @link    https://github.com/Warloxk/drakoon-php-cache
+  */
+
+class Cache
+{
+	public static function Test()
+	{
+		var_dump('CACHE-TEST');
+	}
+}
+
